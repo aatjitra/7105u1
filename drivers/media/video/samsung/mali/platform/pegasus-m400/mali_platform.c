1303e9b48e86910a947ce52f6db164399da964cd
ardi <aatjitra@gmail.com>
2013-04-13 16:09:56 +0800

    mali: disable mali_runtime_resume setting which continuously sets the clock to 266MHz while resuming from idle state

--- drivers/media/video/samsung/mali/platform/pegasus-m400/mali_platform.c
+++ drivers/media/video/samsung/mali/platform/pegasus-m400/mali_platform.c
@@ -522,19 +522,19 @@ static _mali_osk_errcode_t enable_mali_clocks(void)
 #if MALI_PMM_RUNTIME_JOB_CONTROL_ON
 #if MALI_DVFS_ENABLED
 	// set clock rate
-	if (get_mali_dvfs_control_status() != 0 || mali_gpu_clk >= mali_runtime_resume.clk)
+	// if (get_mali_dvfs_control_status() != 0 || mali_gpu_clk >= mali_runtime_resume.clk)
 		mali_clk_set_rate(mali_gpu_clk, GPU_MHZ);
-	else {
-		mali_regulator_set_voltage(mali_runtime_resume.vol, mali_runtime_resume.vol);
-		mali_clk_set_rate(mali_runtime_resume.clk, GPU_MHZ);
-	}
-	if (mali_gpu_clk <= mali_runtime_resume.clk)
+	// else {
+	//	mali_regulator_set_voltage(mali_runtime_resume.vol, mali_runtime_resume.vol);
+	//	mali_clk_set_rate(mali_runtime_resume.clk, GPU_MHZ);
+	// }
+	// if (mali_gpu_clk <= mali_runtime_resume.clk)
 		set_mali_dvfs_current_step(MALI_DVFS_STEPS + 1);
 	/* lock/unlock CPU freq by Mali */
-	if (mali_gpu_clk >= 533)
-		err = cpufreq_lock_by_mali(1400);
-	else if (mali_gpu_clk >= 440)
-		err = cpufreq_lock_by_mali(1200);
+	// if (mali_gpu_clk >= 533)
+	// 	err = cpufreq_lock_by_mali(1400);
+	// else if (mali_gpu_clk >= 440)
+	// 	err = cpufreq_lock_by_mali(1200);
 #else
 	mali_regulator_set_voltage(mali_runtime_resume.vol, mali_runtime_resume.vol);
 	mali_clk_set_rate(mali_runtime_resume.clk, GPU_MHZ);
